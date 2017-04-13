fbe6e678227680dbe6b8379c1b221fba202a86b4
fms-nwise <nwise@Lucravalde03013.local>
2017-04-13 17:10:00 -0400

    -notes

--- FS Javascript/AJAX Basics/readme.js
+++ FS Javascript/AJAX Basics/readme.js
@@ -35,3 +35,56 @@ xhr.send();
 //Or use JSONP - JSON with Padding
 
 //New W3C tech is: CORS - Cross-Origin Resource Sharing
+
+
+//Steps 1-4 can be done using JQuery's .load() function
+$('#ajax').load('sidebar.html');
+
+//api.jquery.com/category/ajax/shorthand-methods/
+
+//a jquery get with ajax looks like...(no selector, call this on Jquery object itself)
+$.get(url, data, callback);
+
+//post also works
+$.post(url, data, callback);
+
+
+$(document).ready(function () {
+    let url="../data/employees.json";
+
+    //data is optional
+    // $.getJSON(url, data, callback);
+    $.getJSON(url, function (response) {
+        let statusHTML = "<ul class='bulleted'>";
+        $.each(response, fuction(index, employee) {
+            if (employee.inoffice === true) {
+                statusHTML += "<li class='in'>";
+            } else {
+                statusHTML += "<li class='out'>";
+            }
+            statusHTML += employee.name + '</li>';
+        });
+        statusHTML += '</ul>';
+        $('#employeeList').html(statusHTML);
+    });
+}); //Can use this jquery instead of putting javascript at the bottom of the page
+
+
+$('thing').each(arrayorobject, callback); //another nice jquery looper
+
+//preventing form submit until some shit
+
+$('form').submit(fucntion(evt) {
+    evt.preventDefault(); //this is the part that disables it until something
+    var url = $(this).attr("action");
+    var formData = $(this).serialize(); //serialize is intersting
+    $.post(url, formData, function(response) {
+        $('#signup').html("<p>Thanks for signing up!</p>");
+    });
+  });
+});
+
+//All this ajax in jquery is building off of...
+$.ajax(url, settings);
+
+//To handle errors use .fail(fuction)
