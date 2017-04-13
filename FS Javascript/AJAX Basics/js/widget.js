3b6ee8973bd19580afc55719130684faee19bb15
fms-nwise <nwise@Lucravalde03013.local>
2017-04-13 15:34:47 -0400

    -changes

--- FS Javascript/AJAX Basics/js/widget.js
+++ FS Javascript/AJAX Basics/js/widget.js
@@ -1,5 +1,4 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '../data/employees.json');
 xhr.onreadystatechange = function () {
   if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
@@ -17,4 +16,26 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function () {
+  if(xhr2.readyState === 4 && xhr2.status === 200) {
+    var rooms = JSON.parse(xhr2.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+xhr2.open('GET', '../data/rooms.json');
+xhr2.send();
