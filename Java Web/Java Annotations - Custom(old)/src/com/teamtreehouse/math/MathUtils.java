ac5ebbecc1515cdd24cbeddd5862b7e465203b0b
fms-nwise <nwise@Lucravalde03013.local>
2017-04-13 09:13:28 -0400

    more

---
+++ Java Web/Java Annotations - Custom(old)/src/com/teamtreehouse/math/MathUtils.java
@@ -0,0 +1,28 @@
+package com.teamtreehouse.math;
+
+import java.awt.geom.Point2D;
+import com.teamtreehouse.docgen.Doc;
+
+public class MathUtils {
+    private static final Double EPSILON = 0.0001;
+
+    public static Double triangleArea(Point2D.Double a, Point2D.Double b, Point2D.Double c) {
+        return 0.0;
+    }
+
+    public static Double distance(Point2D.Double a, Point2D.Double b) {
+        return 0.0;
+    }
+
+    public static Double[] quadraticRoots(int a, int b, int c) {
+        return new Double[]{};
+    }
+
+    public static void epsilon() {
+
+    }
+
+    private static boolean arePointsClose(Point2D.Double a, Point2D.Double b) {
+        return false;
+    }
+}
